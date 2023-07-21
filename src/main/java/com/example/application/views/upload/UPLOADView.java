5152723abf3172665c5023722e1d6d8b8ada8f30
start.vaadin.com <no-reply@vaadin.com>
2023-07-21 09:42:22 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/upload/UPLOADView.java
@@ -0,0 +1,33 @@
+package com.example.application.views.upload;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("UPLOAD")
+@Route(value = "upload", layout = MainLayout.class)
+public class UPLOADView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public UPLOADView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}

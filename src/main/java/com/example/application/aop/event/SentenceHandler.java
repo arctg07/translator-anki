55e7b9ef90ab03c8fb660f4ec844b36641133004
iuriiivanov <iur.ivanov@andersenlab.com>
2023-08-24 10:52:53 +0400

    kafka implementation

--- src/main/java/com/example/application/aop/event/SentenceHandler.java
+++ src/main/java/com/example/application/aop/event/SentenceHandler.java
@@ -4,12 +4,14 @@ import com.example.application.kafka.KafkaProducer;
 import com.example.application.kafka.message.StatisticsMessage;
 import com.example.application.model.EngRusDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author Iurii Ivanov
  */
 
 @RequiredArgsConstructor
+@Slf4j
 public abstract class SentenceHandler implements WordHandler {
 
     private final KafkaProducer producer;
@@ -20,7 +22,12 @@ public abstract class SentenceHandler implements WordHandler {
                 count,
                 sentence
         );
-        producer.sendStatistics(statisticsMessage);
+
+        try {
+            producer.sendStatistics(statisticsMessage);
+        } catch (Exception ex) {
+            log.error("KAFKA ERROR" + "\n" +  ex.getLocalizedMessage());
+        }
     }
 
 }
